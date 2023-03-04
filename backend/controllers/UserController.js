cb24f6d2c807f6f14b39f92b99d66a4550ede7c5
Ryan McHugh <41973210+SyntheticDBX@users.noreply.github.com>
2023-03-04 22:10:58 +0000

    tidied up UserController

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -5,53 +5,52 @@ const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.TOKEN_SECRET, {
     expiresIn: 60 * 60 * 24 * 365,
   });
-
+};
   // user login
-  const loginUser = async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const user = await User.login(username, password);
-      const token = createToken(user._id);
-      res.cookie("jwt", token, {
-        httpOnly: true,
-        maxAge: 60 * 60 * 24 * 365 * 1000,
-      });
-      res.status(200).json({ user: user._id });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
+const loginUser = async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.login(username, password);
+		const token = createToken(user._id);
+		res.cookie("jwt", token, {
+			httpOnly: true,
+			maxAge: 60 * 60 * 24 * 365 * 1000,
+		});
+		res.status(200).json({ user: user._id });
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+};
 
   // user signup
-  const signupUser = async (req, res) => {
-    try {
-      const { username, password, email } = req.body;
-      const user = await User.signup(username, password, email);
-      const token = createToken(user._id);
-      res.cookie("jwt", token, {
-        httpOnly: true,
-        maxAge: 60 * 60 * 24 * 365 * 1000,
-      });
-      res.status(201).json({ user: user._id });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
+const signupUser = async (req, res) => {
+	try {
+		const { username, password, email } = req.body;
+		const user = await User.signup(username, password, email);
+		const token = createToken(user._id);
+		res.cookie("jwt", token, {
+			httpOnly: true,
+			maxAge: 60 * 60 * 24 * 365 * 1000,
+		});
+		res.status(201).json({ user: user._id });
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+};
 
   // user logout
-  const logoutUser = (req, res) => {
-    res.cookie("jwt", "", { maxAge: 1 });
-    res.redirect("/");
-  };
+const logoutUser = (req, res) => {
+	res.cookie("jwt", "", { maxAge: 1 });
+	res.redirect("/");
+};
 
-  const findUserById = async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(404).json({ message: error.message });
-    }
-  }
+const findUserById = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
 };
 
 module.exports = {
