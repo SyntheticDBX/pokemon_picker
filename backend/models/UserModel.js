a45e2ab8af0c3766dedbb03772a4afc7940fa98c
Ryan McHugh <41973210+SyntheticDBX@users.noreply.github.com>
2023-03-04 21:47:33 +0000

    added pokemon type selection for signup

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -19,6 +19,10 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true
+    },
+    starter: {
+        type: String,
+        required: true
     }
 })
 
