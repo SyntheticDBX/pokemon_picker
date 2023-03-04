243780501c22b98879980a7d0275d4931aba1bc9
Ryan McHugh <41973210+SyntheticDBX@users.noreply.github.com>
2023-03-04 23:28:57 +0000

    modified Signup page and app. Created hook for user signup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { useAuthContext } from "./context/AuthContext";
 
-const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-    );
-};
+function App() {
+  const { user } = useAuthContext();
+  return (
+		<div className="app">
+			<BrowserRouter>
+				<div className="pages">
+					<Routes>
+						<Route 
+							path="/" 
+							element={user ? <Home /> : <Navigate to="/login" />} />
+						<Route 
+							path="/login" 
+							element={!user ? <Login /> : <Navigate to="/" />} />
+						<Route 
+							path="/signup" 
+							element={!user ? <Signup /> : <Navigate to="/" />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+  );
+}
 
 export default App;
